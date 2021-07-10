2597ff9cad97cc597ea6eb8fe683daa3c6f06661
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2021-07-09 22:01:22 -0400

    gitignore for webstorm and init pages.

---
+++ pages/application.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+const Application = (): ReactElement => {
+  return (
+    <div>Application Page</div>
+  )
+}
+
+export default Application;
---
+++ pages/home.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+const Home = (): ReactElement => {
+  return (
+    <div>Home Page</div>
+  )
+}
+
+export default Home;
