0712001504be4e1c649fd2bb59afdf21061a343c
thejamielin <jamie.miki.lin@gmail.com>
2021-07-22 21:41:22 -0400

    updated apiClient.ts

--- common/apiClient.ts
+++ common/apiClient.ts
@@ -3,5 +3,43 @@ import Axios from 'axios'
 
 const host = 'https://application-portal.vercel.app'
 export function getAdminById(id: number) {
+  return Axios.get<User>(`/v1/admin/${id}`, { baseURL: host })
+}
+
+export function updateAdminById(id: number, user: User) {
+  Axios.post(`/v1/admin/${id}`, user, { baseURL: host })
+}
+
+export function getAllAdmins() {
+  return Axios.get<Array<User>>(`/v1/admin`, { baseURL: host })
+}
+
+export function getApplicantById(id: number) {
   return Axios.get<User>(`/v1/applicants/${id}`, { baseURL: host })
 }
+
+// do we need to take in body parameters separately
+export function updateApplicantById(id: number, user: User) {
+  Axios.post(`/v1/applicants/${id}`, user, { baseURL: host })
+}
+
+export function getAllApplicants() {
+  return Axios.get<Array<User>>(`/v1/applicants`, { baseURL: host })
+}
+
+export function updateLogin(email: string, password: string) {
+  Axios.post(
+    `/v1/login`,
+    {
+      email: email,
+      password: password,
+    },
+    { baseURL: host }
+  )
+}
+
+// TODO ADD GET AND POST registration functions
+
+// request verification
+
+// status
