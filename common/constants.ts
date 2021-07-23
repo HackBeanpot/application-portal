0d17d96b69d6bc90d774d3e699c737ee8acebaa4
jzhzhang66 <59738880+jzhzhang66@users.noreply.github.com>
2021-07-23 00:01:46 -0400

    Renders each type of question on application page

--- common/constants.ts
+++ common/constants.ts
@@ -6,41 +6,94 @@ import {
   ShirtSize,
   ApplicationStatus,
   RegistrationResponse,
-} from "./types";
-
-export const EXAMPLE_GENDER = Gender.Female;
-export const EXAMPLE_EDUCATION = Education.Undergraduate;
-export const EXAMPLE_ETHNICITIES = [Ethnicity.Asian];
-export const EXAMPLE_SHIRTSIZE = ShirtSize.Small;
-export const EXAMPLE_APPLICATION_STATUS = ApplicationStatus.Declined;
+  Question,
+  Checkboxes,
+  ShortText,
+  Dropdown,
+  LongText,
+  QuestionType,
+} from './types'
+export const EXAMPLE_GENDER = Gender.Female
+export const EXAMPLE_EDUCATION = Education.Undergraduate
+export const EXAMPLE_ETHNICITIES = [Ethnicity.Asian]
+export const EXAMPLE_SHIRTSIZE = ShirtSize.Small
+export const EXAMPLE_APPLICATION_STATUS = ApplicationStatus.Declined
 /**
  * example json User to use for now
  */
-export const EXAMPLE_USER_ID = "user12231";
+export const EXAMPLE_USER_ID = 'user12231'
 export const EXAMPLE_USER: User = {
-  email: "judysu@gmail.com",
-  firstName: "Judy",
-  lastName: "Su",
+  email: 'judysu@gmail.com',
+  firstName: 'Judy',
+  lastName: 'Su',
   id: EXAMPLE_USER_ID,
   gender: EXAMPLE_GENDER,
-  school: "Northeastern",
+  school: 'Northeastern',
   education: EXAMPLE_EDUCATION,
   yearOfEducation: 4,
   ethnicities: EXAMPLE_ETHNICITIES,
   shirtSize: EXAMPLE_SHIRTSIZE,
   applicationStatus: EXAMPLE_APPLICATION_STATUS,
-  major: "cs",
-  minor: "cs",
-  resumeLink: "cs",
-  timeZone: "cs",
-  learningGoals: "cs",
+  major: 'cs',
+  minor: 'cs',
+  resumeLink: 'cs',
+  timeZone: 'cs',
+  learningGoals: 'cs',
   responses: [],
-};
-
+}
 export const EXAMPLE_RESPONSE: RegistrationResponse = {
   userId: EXAMPLE_USER_ID,
   responses: {
-    question1: "Judy Su",
+    question1: 'Judy Su',
     question2: EXAMPLE_ETHNICITIES,
   },
-};
+}
+export const EXAMPLE_CHECKBOX_1: Checkboxes = {
+  id: '1',
+  content: 'Who is the sassiest?',
+  required: true,
+  type: QuestionType.Checkboxes,
+  options: [
+    { name: 'Alex' },
+    { name: 'Jess' },
+    { name: 'Jamie' },
+    { name: 'Karen' },
+  ],
+  maxNumber: 1,
+  minNumber: 1,
+}
+export const EXAMPLE_SHORT_TEXT_1: ShortText = {
+  id: '2',
+  content: "What's your favorite food?",
+  required: false,
+  type: QuestionType.ShortText,
+  minLength: 200,
+  maxLength: 500,
+}
+export const EXAMPLE_DROPDOWN_1: Dropdown = {
+  id: '3',
+  content: "What's your favorite color?",
+  required: true,
+  type: QuestionType.Dropdown,
+  options: [
+    { name: 'blue' },
+    { name: 'green' },
+    { name: 'purple' },
+    { name: 'orange' },
+    { name: 'pink' },
+  ],
+}
+export const EXAMPLE_LONG_TEXT_1: LongText = {
+  id: '4',
+  content: 'Why do you want to participate in Hackbeanpot?',
+  required: true,
+  type: QuestionType.LongText,
+  minLength: 500,
+  maxLength: 1000,
+}
+export const EXAMPLE_QUESTIONS: Array<Question> = [
+  EXAMPLE_CHECKBOX_1,
+  EXAMPLE_SHORT_TEXT_1,
+  EXAMPLE_DROPDOWN_1,
+  EXAMPLE_LONG_TEXT_1,
+]
--- pages/application.tsx
+++ pages/application.tsx
@@ -1,7 +1,87 @@
-import React, { ReactElement } from "react";
-
+import React, { ReactElement } from 'react'
+import {
+  QuestionType,
+  ShortText,
+  LongText,
+  Dropdown,
+  Checkboxes,
+} from '../common/types'
+import { EXAMPLE_QUESTIONS } from '../common/constants'
+// export async function getStaticProps() {
+//   return {
+//     props: { EXAMPLE_QUESTIONS }
+//   }
+// }
+const shortText = (q: ShortText) => {
+  return (
+    <div>
+      <h2>{q.content}</h2>
+      {q.required ? '*' : ''}
+      min-length = {q.minLength}
+      <br />
+      max-length = {q.maxLength}
+      <br />
+      <textarea />
+    </div>
+  )
+}
+const longText = (q: LongText) => {
+  return (
+    <div>
+      <h2>{q.content}</h2>
+      {q.required ? '*' : ''}
+      min-length = {q.minLength}
+      <br />
+      max-length = {q.maxLength}
+      <br />
+      <textarea />
+    </div>
+  )
+}
+const dropDown = (q: Dropdown) => {
+  return (
+    <div>
+      <h2>{q.content}</h2>
+      {q.required ? '*' : ''}
+      {q.options.map((o) => (
+        <ul key={q.id}>{o.name}</ul>
+      ))}
+    </div>
+  )
+}
+const checkBoxes = (q: Checkboxes) => {
+  return (
+    <div>
+      <h2>{q.content}</h2>
+      {q.required ? '*' : ''}
+      {q.options.map((o) => (
+        <ul key={q.id}>{o.name}</ul>
+      ))}
+      min number = {q.minNumber}
+      <br />
+      max number = {q.maxNumber}
+    </div>
+  )
+}
 const Application = (): ReactElement => {
-  return <div>Application Page</div>;
-};
-
-export default Application;
+  return (
+    <>
+      <h1>Application Page</h1>
+      <div>
+        {EXAMPLE_QUESTIONS.map((q) => {
+          switch (q.type) {
+            case QuestionType.ShortText:
+              return shortText(q)
+            case QuestionType.LongText:
+              return longText(q)
+            case QuestionType.Checkboxes:
+              return checkBoxes(q)
+            case QuestionType.Dropdown:
+              return dropDown(q)
+          }
+        })}
+      </div>
+    </>
+  )
+}
+export default Application
