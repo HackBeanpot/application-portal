128a8b79cfcc861d1498f06ad061c6a16e147464
jessicasu77 <jessica.su0807@gmail.com>
2021-07-29 20:34:12 -0400

    added /api to all paths

--- common/apiClient.ts
+++ common/apiClient.ts
@@ -7,32 +7,32 @@ export function getAdminById(id: number) {
 }
 
 export function updateAdminById(id: number, user: User) {
-  Axios.post(`/v1/admin/${id}`, user, { baseURL: host })
+  Axios.post(`/api/v1/admin/${id}`, user, { baseURL: host })
 }
 
 export function getAllAdmins() {
-  return Axios.get<Array<User>>(`/v1/admin`, { baseURL: host })
+  return Axios.get<Array<User>>(`/api/v1/admin`, { baseURL: host })
 }
 
 export function getApplicantById(id: number) {
-  return Axios.get<User>(`/v1/applicants/${id}`, { baseURL: host })
+  return Axios.get<User>(`/api/v1/applicants/${id}`, { baseURL: host })
 }
 
 export function updateApplicantById(id: number, user: User) {
-  Axios.post(`/v1/applicants/${id}`, user, { baseURL: host })
+  Axios.post(`/api/v1/applicants/${id}`, user, { baseURL: host })
 }
 
 export function getAllApplicants() {
-  return Axios.get<Array<User>>(`/v1/applicants`, { baseURL: host })
+  return Axios.get<Array<User>>(`/api/v1/applicants`, { baseURL: host })
 }
 
 export function getWelcome() {
-  return Axios.get<string>(`/v1/welcome`, { baseURL: host })
+  return Axios.get<string>(`/api/v1/welcome`, { baseURL: host })
 }
 
 const welcomeMessage = ''
 export function postWelcome() {
-  Axios.post(`/v1/welcome`, welcomeMessage, { baseURL: host })
+  Axios.post(`/api/v1/welcome`, welcomeMessage, { baseURL: host })
 }
 
 // status functions need bearer token
