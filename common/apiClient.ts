12617b5ba56bf4dd47628c2997bbc8b3d1e53658
jessicasu77 <jessica.su0807@gmail.com>
2021-07-30 11:06:44 -0400

    deleted { baseURL: host } arguments

--- common/apiClient.ts
+++ common/apiClient.ts
@@ -3,31 +3,31 @@ import Axios from 'axios'
 
 const host = 'https://application-portal.vercel.app'
 export function getAdminById(id: number) {
-  return Axios.get<User>(`/api/v1/admin/${id}`, { baseURL: host })
+  return Axios.get<User>(`/api/v1/admin/${id}`)
 }
 
 export function updateAdminById(id: number, user: User) {
-  Axios.post(`/api/v1/admin/${id}`, user, { baseURL: host })
+  Axios.post(`/api/v1/admin/${id}`, user)
 }
 
 export function getAllAdmins() {
-  return Axios.get<Array<User>>(`/api/v1/admin`, { baseURL: host })
+  return Axios.get<Array<User>>(`/api/v1/admin`)
 }
 
 export function getApplicantById(id: number) {
-  return Axios.get<User>(`/api/v1/applicants/${id}`, { baseURL: host })
+  return Axios.get<User>(`/api/v1/applicants/${id}`)
 }
 
 export function updateApplicantById(id: number, user: User) {
-  Axios.post(`/api/v1/applicants/${id}`, user, { baseURL: host })
+  Axios.post(`/api/v1/applicants/${id}`, user)
 }
 
 export function getAllApplicants() {
-  return Axios.get<Array<User>>(`/api/v1/applicants`, { baseURL: host })
+  return Axios.get<Array<User>>(`/api/v1/applicants`)
 }
 
 export function getWelcome() {
-  return Axios.get<string>(`/api/v1/welcome`, { baseURL: host })
+  return Axios.get<string>(`/api/v1/welcome`)
 }
 
 const welcomeMessage = ''
