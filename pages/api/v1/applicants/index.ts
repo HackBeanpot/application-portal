54d3b4b94c02f64984713bb23675a7d3e66b5e15
Alexander Takayama <alex@takayamasan.com>
2021-07-30 01:41:30 -0400

    add basic sign-in with email (#20)
    * add basic sign-in with email
    
    * added auth to backend; if users aren't logged in they can't access the application
    
    Co-authored-by: Karen Li <li.kar@northeastern.edu>

--- pages/api/v1/applicants/index.ts
+++ pages/api/v1/applicants/index.ts
@@ -1,7 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { EXAMPLE_USER } from '../../../../common/constants'
+import { protect } from '../../../../server/protect'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default protect(function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   switch (req.method) {
     // get all the users
     // in the future, we probably want pagination on this (there will be 100+ applicants lol)
@@ -11,4 +15,4 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       // only allow post on the /api/v1/applicants/:id route (for updating)
       return res.status(405).setHeader('Allow', 'GET').send(undefined)
   }
-}
+})
--- pages/api/v1/status.ts
+++ pages/api/v1/status.ts
@@ -1,7 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { EXAMPLE_APPLICATION_STATUS } from '../../../common/constants'
+import { protect } from '../../../server/protect'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default protect(function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   switch (req.method) {
     case 'GET':
       // stringify bc for some reason, next was removing the quotes in the response
@@ -11,4 +15,4 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     default:
       return res.status(405).setHeader('Allow', 'GET, POST').send(undefined)
   }
-}
+})
