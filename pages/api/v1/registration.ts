88daaa8aa36a5bf21b0658b065b243c784f50f6b
jessicasu77 <jessica.su0807@gmail.com>
2021-08-03 21:51:24 -0400

    started get /registration

--- pages/api/v1/registration.ts
+++ pages/api/v1/registration.ts
@@ -1,14 +1,35 @@
+<<<<<<< HEAD
 import { NextApiRequest, NextApiResponse } from 'next';
 import { EXAMPLE_RESPONSE } from '../../../common/constants';
 import { protect } from '../../../server/protect';
+=======
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getSession, useSession } from 'next-auth/client'
+import { EXAMPLE_RESPONSE } from '../../../common/constants'
+import { connectToDatabase } from '../../../server/mongoDB'
+import { protect } from '../../../server/protect'
+import { useSignIn } from '../../application'
+>>>>>>> started get /registration
 
-export default protect(function handler(
+export default protect(async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   switch (req.method) {
     case 'GET':
+<<<<<<< HEAD
       return res.status(200).json(EXAMPLE_RESPONSE);
+=======
+      const session = (await getSession({ req }))!
+      session.user?.email
+
+      const { client, db } = await connectToDatabase()
+      const data = await db
+        .collection('your colelciton here')
+        .findOne({ email: session.user?.email })
+
+      return res.status(200).json(data)
+>>>>>>> started get /registration
     case 'POST':
       return res.status(201).send(undefined);
     default:
