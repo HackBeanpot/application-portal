f0bf8a7e29ff4a24bb9f6dadb78118e5bdb2c029
Karen Li <li.kar@northeastern.edu>
2021-08-12 22:57:00 -0400

    ability to make diff types of questions

--- common/questions.ts
+++ common/questions.ts
@@ -9,14 +9,99 @@ import {
   RegistrationResponse,
   QuestionIdToResponseMap,
   QuestionIdToQuestionContent,
+  QuestionType,
+  Checkboxes,
+  ShortText,
+  LongText,
+  Dropdown,
+  Question,
 } from './types'
 
-const questionCount = 0
+let questionCount = 0
+// constants for min/max length of q + others
+const minNumberCheck = 0
+const maxLengthShort = 500
+const minLengthShort = 0
+const maxLengthLong = 3000
+const minLengthLong = 0
 
-// convenience constructors for questions
+// convenience constructors for questions (constructors in java)
+function makeCheckbox(
+  content: string,
+  options: Array<string>,
+  required: boolean,
+  maxNumberCheck: number
+): Checkboxes {
+  questionCount++
+  return {
+    type: QuestionType.Checkboxes,
+    options: options.map((name) => ({ name })),
+    maxNumber: maxNumberCheck,
+    minNumber: minNumberCheck,
 
-// expor big object mapping of id -> question
-export const Questions: QuestionIdToQuestionContent = {
-  questionid1: EXAMPLE_CHECKBOX_1,
-  questionid2: EXAMPLE_DROPDOWN_1,
+    content: content,
+    id: String(questionCount), // need to access questionID from questionidtoquestioncontent
+    required: required,
+  }
 }
+
+function makeShortText(content: string, required: boolean): ShortText {
+  questionCount++
+  return {
+    type: QuestionType.ShortText,
+    maxLength: maxLengthShort,
+    minLength: minLengthShort,
+
+    content: content,
+    id: String(questionCount), // need to access questionID from questionidtoquestioncontent
+    required: required,
+  }
+}
+
+function makeDropdown(
+  content: string,
+  options: Array<string>,
+  required: boolean
+): Dropdown {
+  questionCount++
+  return {
+    type: QuestionType.Dropdown,
+    options: options.map((name) => ({ name })),
+
+    content: content,
+    id: String(questionCount), // need to access questionID from questionidtoquestioncontent
+    required: required,
+  }
+}
+
+function makeLongText(content: string, required: boolean): LongText {
+  questionCount++
+  return {
+    type: QuestionType.LongText,
+    maxLength: maxLengthLong,
+    minLength: minLengthLong,
+
+    content: content,
+    id: String(questionCount), // need to access questionID from questionidtoquestioncontent
+    required: required,
+  }
+}
+
+// write questions for portal here
+export const QUESTIONS: Array<Question> = [
+  makeShortText('What is your name?', true),
+  makeDropdown(
+    'What year are you?',
+    ['Freshman', 'Sophomore', 'Junior', 'Senior', 'Other'],
+    true
+  ),
+  makeLongText('What is your favorite food?', false),
+  makeCheckbox(
+    'What languages do you speak?',
+    ['English', 'Spanish', 'Mandarin', 'Hindi', 'Other'],
+    false,
+    5
+  ),
+]
+
+// constructors for people writing the portal qs
