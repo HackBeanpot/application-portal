7a2f75e9f35f6fda78daca7b02a376963cb5fdd7
meebs <alex@takayamasan.com>
2021-09-27 18:49:59 -0400

    styles in progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Provider } from 'next-auth/client';
+import '../styles/main.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
