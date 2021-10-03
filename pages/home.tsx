9e007c51bbfd8a19f37f7bc06fe4f698066476a3
meebs <alex@takayamasan.com>
2021-10-03 16:39:25 -0400

    remove home.tsx (unused page)

--- pages/home.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactElement } from 'react';
-import { useSessionOrRedirect } from '../hooks/useSessionOrRedirect';
-
-const Home = (): ReactElement => {
-  useSessionOrRedirect();
-  return <div>Home Page</div>;
-};
-
-export default Home;
