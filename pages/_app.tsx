5d42216056800f359a66a1ee7a44a244f6294528
meebs <alex@takayamasan.com>
2021-10-25 18:49:49 -0400

    remove console log and force a redeploy!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import { SessionProvider } from 'next-auth/react';
 import '../styles/main.scss';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  console.log(session);
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
