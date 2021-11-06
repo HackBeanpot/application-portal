fd0a5bdced88939cb341f4e1ecebb63cae3144c0
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2021-11-05 19:40:45 -0700

    Fix data formatting.

--- pages/api/v1/stats.ts
+++ pages/api/v1/stats.ts
@@ -1,6 +1,7 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 import { connectToDatabase } from '../../../server/mongoDB';
 import { protect } from '../../../server/protect';
+import { User } from '../../../common/types';
 
 const statsHandler: NextApiHandler = async (req, res) => {
   switch (req.method) {
@@ -17,6 +18,7 @@ const getStats: NextApiHandler = async (
   res: NextApiResponse
 ) => {
   const { userDataCollection } = await connectToDatabase();
+
   const statusData = await userDataCollection
     .aggregate([{ $group: { _id: '$applicationStatus', count: { $sum: 1 } } }])
     .toArray();
@@ -29,7 +31,37 @@ const getStats: NextApiHandler = async (
     .aggregate([{ $group: { _id: '$yearOfEducation', count: { $sum: 1 } } }])
     .toArray();
 
-  return res.status(200).json({ statusData, shirtData, yearData });
+  const resData = convertData(
+    ['status', 'shirt', 'year'],
+    [statusData, shirtData, yearData],
+    {}
+  );
+
+  const total = await userDataCollection.find().toArray();
+
+  resData['Total'] = total.length;
+
+  return res.status(200).json(resData);
+};
+
+const convertData = (
+  cat: string[],
+  collections: User[][],
+  resData: Record<string, number>
+) => {
+  cat.forEach((c, ind) => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    collections[ind].forEach((category: { _id: string; count: number }) => {
+      const id = category._id ? category._id : `Unknown ${c}`;
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      resData[id] = category.count;
+    });
+  });
+
+  return resData;
 };
 
 export default protect(statsHandler);
+// export default statsHandler;
