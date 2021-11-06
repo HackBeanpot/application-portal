fe0b47bcb043ee4d5f59112fadc15a6171542775
meebs <alex@takayamasan.com>
2021-11-06 04:08:36 -0400

    add favicon and page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import Head from 'next/head';
 import '../styles/main.scss';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>HackBeanpot Application Portal</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
