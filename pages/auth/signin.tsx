a6dd2825110675eb8c83a83169987f9f3c5b9dfd
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2021-11-11 22:17:31 -0800

    basic styling.

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,4 +1,4 @@
-import {
+import React, {
   ChangeEvent,
   FormEvent,
   ReactElement,
@@ -10,6 +10,7 @@ import { getSession, signIn, SignInOptions, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { SignInResponse } from 'next-auth/react/types';
 import { GetServerSideProps } from 'next';
+import Image from 'next/image';
 
 const SignIn = (): ReactElement => {
   const [email, setEmail] = useState<string>('');
@@ -42,33 +43,44 @@ const SignIn = (): ReactElement => {
   };
 
   return (
-    <Form
-      name="signin"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      autoComplete="off"
-      onFinish={async (e) => await loginOnPress(e, { redirect: false, email })}
-    >
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[
-          {
-            required: true,
-            type: 'email',
-            message: 'Please input your email!',
-          },
-        ]}
+    <div className="signinPage">
+      <div className="logoWrapper">
+        <Image
+          className="signinLogo"
+          width={300}
+          height={270}
+          src="/logo_big.png"
+        />
+      </div>
+      <Form
+        className="signinForm"
+        name="signin"
+        autoComplete="off"
+        onFinish={async (e) =>
+          await loginOnPress(e, { redirect: false, email })
+        }
       >
-        <Input onChange={updateEmail} value={email} />
-      </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              type: 'email',
+              message: 'Please input a valid email!',
+            },
+          ]}
+        >
+          <Input onChange={updateEmail} value={email} />
+        </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item className="submitButton">
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
