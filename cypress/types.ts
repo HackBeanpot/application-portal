b58b704a0f5193487db0e25d3c7936421c4484f9
meebs <alex@takayamasan.com>
2022-01-05 15:52:15 -0800

    Review comments

--- cypress/types.ts
+++ cypress/types.ts
@@ -1,3 +1,10 @@
+/**
+ * Represents the argument for the `createUserInBackend` task.
+ *
+ * When using the task, first declare the arg with this type, so that
+ * if it gets changed later, all the usages of the task can be easily
+ * found and updated.
+ */
 export type CreateUserInBackendArg = {
   email: string;
   token: string;
