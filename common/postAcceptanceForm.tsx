c0e94b09b1e39e0f40da5564927f96daf9739a28
jzhzhang66 <jzhzhang66@gmail.com>
2022-01-13 23:12:31 -0500

    finished adding question info

--- common/postAcceptanceForm.tsx
+++ common/postAcceptanceForm.tsx
@@ -5,9 +5,26 @@ import { QuestionDefinition, QuestionSection } from './types';
 export const PostAcceptanceForm: Array<QuestionSection | QuestionDefinition> = [
   makeSection(
     <>Acceptance Form</>,
-    "Congrats on being accepted to HackBeanpot! We're so excited to see you soon. Please answer the following questions to help us finalize logistics of making the event as great as it can be for our attendees! *NOTE: We will only be able to guarantee swag for the first 200 hackers who complete this form and qualify for domestic shipping. Thanks for your understanding."
+    <div>
+      <p>
+        Congrats on being accepted to HackBeanpot! We're so excited to see you soon. Please answer
+        the following questions to help us finalize logistics of making the event as great as it can
+        be for our attendees!
+      </p>
+      *NOTE: We will only be able to guarantee swag for the first 200 hackers who complete this form
+      and qualify for domestic shipping. Thanks for your understanding.
+    </div>
   ),
-  makeShortText('email', 'Email address' + <br/> + "Please use the email address you applied to HackBeanpot with - this should be the one where you received your acceptance email!", true, 'Email address'),
+  makeShortText(
+    'email',
+    <div>
+      <p>Email address</p>
+      Please use the email address you applied to HackBeanpot with - this should be the one where
+      you received your acceptance email!
+    </div>,
+    true,
+    'Email address'
+  ),
   makeShortText('name', 'Full name', true, 'First Last'),
   makeDropdown(
     'attending',
@@ -20,14 +37,33 @@ export const PostAcceptanceForm: Array<QuestionSection | QuestionDefinition> = [
   makeSection(<>Above 18 Signature</>),
   makeShortText(
     'adultSignature',
-    'By typing my full legal name below, I acknowledge that this represents my legal signature and that I have read and agreed to the terms and conditions stated in the Participant Waiver. PARTICIPANT WAIVER: bit.ly/hbp2022-waiver',
+    <div>
+      <p>
+        By typing my full legal name below, I acknowledge that this represents my legal signature
+        and that I have read and agreed to the terms and conditions stated in the Participant
+        Waiver.
+      </p>
+      PARTICIPANT WAIVER:{' '}
+      <a href="https://bit.ly/hbp2022-waiver" target="_blank">
+        bit.ly/hbp2022-waiver
+      </a>
+    </div>,
     true,
     'First Last'
   ),
-  makeSection(<>Under 18 Signature</>, 'PARTICIPANT WAIVER: bit.ly/hbp2022-waiver'),
+  makeSection(<>Under 18 Signature</>),
   makeShortText(
     'minorSignature',
-    'Attendee: I acknowledge that I am above 13 and that I have read and agreed to the terms and conditions stated in the Participant Waiver. PARTICIPANT WAIVER: bit.ly/hbp2022-waiver',
+    <div>
+      <p>
+        Attendee: I acknowledge that I am above 13 and that I have read and agreed to the terms and
+        conditions stated in the Participant Waiver.
+      </p>
+      PARTICIPANT WAIVER:{' '}
+      <a href="https://bit.ly/hbp2022-waiver" target="_blank">
+        bit.ly/hbp2022-waiver
+      </a>
+    </div>,
     true,
     'First Last'
   ),
@@ -37,10 +73,15 @@ export const PostAcceptanceForm: Array<QuestionSection | QuestionDefinition> = [
     true,
     'First Last'
   ),
-  makeSection(<>Now the fun (free) stuff!</>, "Even though our event will be mostly virtual, we still plan on getting our awesome swag to our attendees! We will be having Swag Outpost stations on certain days at Northeastern University's campus in Boston. For those unable to pick up in Boston, we will try our best to ship swag to you if you're within the continental US."),
+  makeSection(
+    <>Now the fun (free) stuff!</>,
+    "Even though our event will be mostly virtual, we still plan on getting our awesome swag to our attendees! We will be having Swag Outpost stations on certain days at Northeastern University's campus in Boston. For those unable to pick up in Boston, we will try our best to ship swag to you if you're within the continental US."
+  ),
   makeCheckbox(
     'swag',
-    'Which of the following swag items would you like to receive?' + <br/> +"In the spirit of sustainability, we want to make sure that all of our swag items are useful and appreciated by everyone! Please help us by only opting in to the items that you would actually like.",
+    'Which of the following swag items would you like to receive?' +
+      <br /> +
+      'In the spirit of sustainability, we want to make sure that all of our swag items are useful and appreciated by everyone! Please help us by only opting in to the items that you would actually like.',
     [
       'T-shirt: our annual event shirt',
       'Sticker sheet: classic HBP logo sticker with special space themed stickers',
@@ -53,23 +94,52 @@ export const PostAcceptanceForm: Array<QuestionSection | QuestionDefinition> = [
   ),
   makeShortText(
     'accomodations',
-    'Are there any accommodations (e.g. dietary restrictions) you would need from us?' + <br/> + "For snacks!",
+    <div>
+      <p>Are there any accommodations (e.g. dietary restrictions) you would need from us?</p>
+      For snacks!
+    </div>,
     true,
     'accomodations'
   ),
   makeDropdown(
     'pickUpSwag',
-    'If you are in the Boston area, would you be interested in picking up your swag box at a location on the Northeastern University campus in January?' + <br/> + "It would really help us out :') Shipping is expensive these days but we want to get you the BEST swag out there <3",
+    <div>
+      <p>
+        If you are in the Boston area, would you be interested in picking up your swag box at a
+        location on the Northeastern University campus in January?
+      </p>
+      It would really help us out :') Shipping is expensive these days but we want to get you the
+      BEST swag out there!
+    </div>,
     ['Yes', 'No/Not in Boston'],
     true,
     'Yes'
   ),
   makeLongText(
     'address',
-    'Otherwise, what physical address would you like your swag to be shipped to? Please include the full name the package should be addressed to. *Note: At this time we can only ship to addresses in the US. Please write N/A if not in the US.' + <br/> + "Disclaimer: This information will not be shared with anyone outside of our organizing team and will only be used for shipping. Please note that the package will take two weeks to ship and will arrive mid-February.",
+    <div>
+      <p>
+        Otherwise, what physical address would you like your swag to be shipped to? Please include
+        the full name the package should be addressed to. *Note: At this time we can only ship to
+        addresses in the US. Please write N/A if not in the US.
+      </p>
+      Disclaimer: This information will not be shared with anyone outside of our organizing team and
+      will only be used for shipping. Please note that the package will take two weeks to ship and
+      will arrive mid-February.
+    </div>,
     true
   ),
-  makeSection(<>Planet Mission Crew Sorting!</>, "Three… two… one… liftoff! In the spirit of our space theme, each attendees will be apart of a planet mission crew! Planet mission crews will be a space for hackers to meet and socialize with other attendees outside of your project team through the vacuum of empty interplanetary space. Throughout the weekend, you'll have chances to win points for your new homeworld at different workshops/activities and compete for the Space Race to the moon."),
+  makeSection(
+    <>Planet Mission Crew Sorting!</>,
+    <div>
+      <p>Three… two… one… liftoff!</p>
+      In the spirit of our space theme, each attendees will be apart of a planet mission crew!
+      Planet mission crews will be a space for hackers to meet and socialize with other attendees
+      outside of your project team through the vacuum of empty interplanetary space. Throughout the
+      weekend, you'll have chances to win points for your new homeworld at different
+      workshops/activities and compete for the Space Race to the moon.
+    </div>
+  ),
   makeDropdown(
     'careerInTech',
     'What career in tech most interests you?',
