fc18f49ba0208da6f2c8f9f69d854bb7b34c16a1
jzhzhang66 <jzhzhang66@gmail.com>
2022-10-09 16:44:34 -0400

    TESTS ARE WORKING IM GONNA CRY:')))))))) YAYAYAYAY

--- tests/ApplicationStatusDialogue.test.tsx
+++ tests/ApplicationStatusDialogue.test.tsx
@@ -20,6 +20,20 @@ describe('Application Status Dialogue', () => {
         'Thank you for submitting your application! We will notify you by email when there is a change in your application status.'
       );
     });
+    it('renders the Submitted component when registrationState is closed', () => {
+      render(
+        <ApplicationStatusDialogue
+          applicationStatus={ApplicationStatus.Submitted}
+          registrationClosed={new Date('2052-10-01T22:40:02.000Z')}
+          registrationState={RegistrationState.Closed}
+        />
+      );
+
+      const submittedDialogText = screen.getByTestId('submitted-dialog-text');
+      expect(submittedDialogText.textContent).toBe(
+        'Thank you for submitting your application! We will notify you by email when there is a change in your application status.'
+      );
+    });
   });
 
   describe('has a incomplete application status ', () => {
