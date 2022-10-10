8341c3615413d541a5115bff892cd969462a012a
jzhzhang66 <jzhzhang66@gmail.com>
2022-10-09 21:35:36 -0400

    starting status dialogue

---
+++ tests/StatusDialogue.test.tsx
@@ -0,0 +1,9 @@
+import { describe, expect, it } from '@jest/globals';
+import { render, screen } from '@testing-library/react';
+import { RSVPStatus } from '../common/types';
+import { StatusDialogue } from '../components/dashboard/StatusDialogue';
+
+describe('Rsvp Dialogue', () => {
+  it('renders the ApplyLater component when registrationState is BeforeOpen', () => {
+  });
+});
