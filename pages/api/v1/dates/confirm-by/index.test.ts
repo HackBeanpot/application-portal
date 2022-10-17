00110fb5658674597982ea20763e074f133ec45a
surbhi <surbhigulati2000@gmail.com>
2022-10-16 23:59:30 -0400

    added POST test for confirm-by

--- pages/api/v1/dates/confirm-by/index.test.ts
+++ pages/api/v1/dates/confirm-by/index.test.ts
@@ -4,6 +4,7 @@ import { DateSingleton, SingletonType } from '../../../../../common/types';
 
 let ctx: JestMongoCtx;
 const initialConfirmByDate = '2022-10-01T22:40:02.000Z';
+const updatedConfirmByDate = '2022-10-04T22:40:02.000Z';
 
 beforeEach(async () => {
   ctx = await jestConnectToDatabase();
@@ -26,5 +27,25 @@ describe('confirmByDate', () => {
       type: SingletonType.ConfirmBy,
     })) as DateSingleton;
     expect(getConfirmByDate.value).toBe(initialConfirmByDate);
+  });
+
+  it('is posted correctly to mongodb', async () => {
+    let getConfirmByDate = (await ctx.serverDb.singletonDataCollection.findOne({
+      type: SingletonType.ConfirmBy,
+    })) as DateSingleton;
+    expect(getConfirmByDate.value).toBe(initialConfirmByDate);
+
+    await ctx.serverDb.singletonDataCollection.updateOne(
+      { type: SingletonType.ConfirmBy },
+      {
+        $set: { value: updatedConfirmByDate },
+      },
+      { upsert: true }
+    );
+
+    getConfirmByDate = (await ctx.serverDb.singletonDataCollection.findOne({
+      type: SingletonType.ConfirmBy,
+    })) as DateSingleton;
+    expect(getConfirmByDate.value).toBe(updatedConfirmByDate);
   });
 });
