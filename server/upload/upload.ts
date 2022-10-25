e24e43aa3fa4084ec9db882b513b94fb9383b030
Dean Frame <dacframe@gmail.com>
2022-10-25 01:36:03 -0400

    remove json parse

--- server/upload/upload.ts
+++ server/upload/upload.ts
@@ -21,9 +21,7 @@ async function uploadFile(
       projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
       credentials: {
         client_email: process.env.GOOGLE_CLOUD_EMAIL,
-        private_key: JSON.parse(
-          (process.env.GOOGLE_CLOUD_PRIVATE_KEY as string).replace(/\\n/g, '\n')
-        ),
+        private_key: (process.env.GOOGLE_CLOUD_PRIVATE_KEY as string).replace(/\\n/g, '\n'),
       },
     });
   }
