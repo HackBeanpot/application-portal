3fe6b014777de03b4182b7a4e7a17d7f510a3e54
Dean Frame <dacframe@gmail.com>
2022-11-01 13:56:24 -0400

    Fix stats page (#191)
    * fix stats page
    
    * fix stats page
    
    * reduce space complexity, shoutout my algo prof
    
    * render dows with 0s
    
    * render rows with 0s

--- pages/api/v1/stats.ts
+++ pages/api/v1/stats.ts
@@ -13,10 +13,7 @@ const statsHandler: NextApiHandler = async (req, res) => {
   }
 };
 
-const getStats: NextApiHandler = async (
-  req: NextApiRequest,
-  res: NextApiResponse
-) => {
+const getStats: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const adminCheck = await isAdmin(req);
   if (!adminCheck) {
     return res.status(401).send({ message: 'User is not an admin' });
@@ -28,13 +25,19 @@ const getStats: NextApiHandler = async (
     .aggregate([{ $group: { _id: '$applicationStatus', count: { $sum: 1 } } }])
     .toArray();
 
+  const statuses = ['Incomplete', 'Submitted'];
+  const statusDataWithEmpties = statuses.map((status: string) => {
+    return {
+      _id: status,
+      count: statusData.find((e) => e._id === status)?.count ?? 0,
+    };
+  });
+
   const shirtData = await userDataCollection
     .aggregate([
       {
         $group: {
-          _id: {
-            $arrayElemAt: ['$responses', 11],
-          },
+          _id: '$applicationResponses.shirtSize',
           count: { $sum: 1 },
         },
       },
@@ -45,58 +48,38 @@ const getStats: NextApiHandler = async (
   const orderedShirtData = ABBV_SHIRT_SIZE.map((size: string) => {
     return {
       _id: `T-shirt ${size}`,
-      count: shirtData.find((e) => e._id === (size === 'Unknown' ? null : size))
-        ?.count,
+      count: shirtData.find((e) => e._id === (size === 'Unknown' ? null : size))?.count ?? 0,
     };
   });
 
-  const yearData = await userDataCollection
-    .aggregate([
-      {
-        $group: {
-          _id: {
-            $arrayElemAt: ['$responses', 7],
-          },
-          count: { $sum: 1 },
-        },
-      },
-    ])
+  const decisionStatusData = await userDataCollection
+    .aggregate([{ $group: { _id: '$decisionStatus', count: { $sum: 1 } } }])
     .toArray();
 
-  const ABBV_YEAR = [
-    '1st year',
-    '2nd year',
-    '3rd year',
-    '4th year',
-    '5th year+',
-    'Unknown year',
-  ];
-  const orderedYearData = ABBV_YEAR.map((year: string) => {
+  const decisionStatuses = ['Admitted', 'Waitlisted', 'Declined', 'Undecided'];
+  const decisionStatusDataWithEmpties = decisionStatuses.map((decisionStatus) => {
     return {
-      _id: `${year}`,
-      count: yearData.find(
-        (e) => e._id === (year === 'Unknown year' ? null : year)
-      )?.count,
+      _id: decisionStatus,
+      count:
+        decisionStatusData.find(
+          (e) => e._id === (decisionStatus === 'Undecided' ? null : decisionStatus)
+        )?.count ?? 0,
     };
   });
 
   const resData = convertData(
-    ['status', 'shirt', 'year'],
-    [statusData, orderedShirtData, orderedYearData],
+    ['status', 'shirt', 'decisionStatus'],
+    [statusDataWithEmpties, orderedShirtData, decisionStatusDataWithEmpties],
     {}
   );
 
   const total = await userDataCollection.find().toArray();
-  resData['Total'] = total.length;
+  resData['Total applicants'] = total.length;
 
   return res.status(200).json(resData);
 };
 
-const convertData = (
-  cat: string[],
-  collections: Document[][],
-  resData: Record<string, number>
-) => {
+const convertData = (cat: string[], collections: Document[][], resData: Record<string, number>) => {
   cat.forEach((c, ind) => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
