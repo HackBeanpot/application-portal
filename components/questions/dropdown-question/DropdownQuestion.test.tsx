4fdb97c11442e1bd6c067b8377be7dd1962beb1b
Surbhi Gulati <53664026+surbhi-gulati@users.noreply.github.com>
2022-11-01 20:38:37 -0700

    Fix dropdown tests

--- components/questions/dropdown-question/DropdownQuestion.test.tsx
+++ components/questions/dropdown-question/DropdownQuestion.test.tsx
@@ -1,40 +1,49 @@
 import { describe, expect, it } from '@jest/globals';
-// import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import DropdownQuestion from './DropdownQuestion';
 import { Dropdown, QuestionResponse, QuestionType } from '../../../common/types';
 import { Form } from 'antd';
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 describe('CheckboxesQuestion component', () => {
+  const n = (name: string) => ({ name });
   const exampleContent = 'How old are you?';
-
-  jest.mock("./DropdownQuestion", () => () => {
-    const n = (name: string) => ({ name });
-    const exampleQuestion : Dropdown = {
-      field: 'adult',
-      type: QuestionType.Dropdown,
-      id: '1',
-      content: exampleContent,
-      options: ['1', '2', '3', '4+'].map(n),
-      required: true,
-    };
-    const [exampleForm] = Form.useForm<Record<string, QuestionResponse>>();
-
-    return <DropdownQuestion 
-      question={exampleQuestion}
-      form={exampleForm}
-      disabled={false}
-      data-testid="dropdown-question-mock"
-    />;
-  });
+  const exampleQuestion: Dropdown = {
+    field: 'adult',
+    type: QuestionType.Dropdown,
+    id: '1',
+    content: exampleContent,
+    options: ['1', '2', '3', '4+'].map(n),
+    required: true,
+  };
   
-  // it('renders', () => {
-  //   render(<DropdownQuestion />);
-  //   const confirmedDialogText = screen.getByTestId('dropdown-question');
+  const DropdownQuestionWrapper = () => {
+    const form = Form.useForm<Record<string, QuestionResponse>>()[0];
 
-  //   expect(confirmedDialogText).toContain(exampleContent);
-  //   expect(confirmedDialogText).toContain("1");
-  //   expect(confirmedDialogText).toContain("2");
-  //   expect(confirmedDialogText).toContain("3");
-  //   expect(confirmedDialogText).toContain("4");
-  // });
+    return <DropdownQuestion question={exampleQuestion} form={form} disabled={false} />;
+  };
+
+  it('renders', () => {
+    render(<DropdownQuestionWrapper />);
+    const confirmedDialogText = screen.getByTestId('dropdown-question');
+
+    expect(confirmedDialogText.textContent).toContain(exampleContent);
+    expect(confirmedDialogText.textContent).toContain("1");
+    expect(confirmedDialogText.textContent).toContain("2");
+    expect(confirmedDialogText.textContent).toContain("3");
+    expect(confirmedDialogText.textContent).toContain("4");
+  });
 });
