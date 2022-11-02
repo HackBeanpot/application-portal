a8ec383306c34b0a87fa8940dc391a01b4e9640c
Surbhi Gulati <53664026+surbhi-gulati@users.noreply.github.com>
2022-11-01 18:34:25 -0700

    Checkbox test errors bc hook call outside FC

--- components/questions/checkboxes-question/CheckboxesQuestion.test.tsx
+++ components/questions/checkboxes-question/CheckboxesQuestion.test.tsx
@@ -1,39 +1,36 @@
-// import { describe, expect, it } from '@jest/globals';
-// import { render, screen } from '@testing-library/react';
-// import CheckboxesQuestion from './CheckboxesQuestion';
-// import { Checkboxes, QuestionResponse, QuestionType } from '../../../common/types';
-// import { FormInstance } from 'antd';
-import { describe } from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
+import { render, screen } from '@testing-library/react';
+import CheckboxesQuestion from './CheckboxesQuestion';
+import { Checkboxes, QuestionResponse, QuestionType } from '../../../common/types';
+import { Form } from 'antd';
 
 describe('CheckboxesQuestion component', () => {
-  // const n = (name: string) => ({ name });
-  // const exampleContent = 'Your top two most proficient languages. Select at least 1, but no more than 2.';
-  // const exampleQuestion: Checkboxes = {
-  //   field: 'education',
-  //   type: QuestionType.Checkboxes,
-  //   id: '1',
-  //   content: exampleContent,
-  //   minNumber: 1,
-  //   maxNumber: 2,
-  //   options: ['English', 'Java', 'Typescript', 'Japanese'].map(n),
-  //   required: true,
-  // };
-  // const exampleForm: FormInstance<Record<string, QuestionResponse>> = { 
-  //   new FormInstance(Record<" ", QuestionResponse>>()) 
-  // };
-  
-  // it('renders', () => {
-  //   render(<CheckboxesQuestion 
-  //     question={exampleQuestion}
-  //     form={exampleForm}
-  //     disabled={false}
-  //   />);
-  //   const confirmedDialogText = screen.getByTestId('checkboxes-question');
+  const n = (name: string) => ({ name });
+  const exampleContent = 'Your top two most proficient languages. Select at least 1, but no more than 2.';
+  const exampleQuestion: Checkboxes = {
+    field: 'education',
+    type: QuestionType.Checkboxes,
+    id: '1',
+    content: exampleContent,
+    minNumber: 1,
+    maxNumber: 2,
+    options: ['English', 'Java', 'Typescript', 'Japanese'].map(n),
+    required: true,
+  };
+  const [exampleForm] = Form.useForm<Record<string, QuestionResponse>>();
 
-  //   expect(confirmedDialogText).toContain(exampleContent);
-  //   expect(confirmedDialogText).toContain("English");
-  //   expect(confirmedDialogText).toContain("Java");
-  //   expect(confirmedDialogText).toContain("Typescript");
-  //   expect(confirmedDialogText).toContain("Japanese");
-  // });
+  it('renders', () => {
+    render(<CheckboxesQuestion 
+      question={exampleQuestion}
+      form={exampleForm}
+      disabled={false}
+    />);
+    const confirmedDialogText = screen.getByTestId('checkboxes-question');
+
+    expect(confirmedDialogText).toContain(exampleContent);
+    expect(confirmedDialogText).toContain("English");
+    expect(confirmedDialogText).toContain("Java");
+    expect(confirmedDialogText).toContain("Typescript");
+    expect(confirmedDialogText).toContain("Japanese");
+  });
 });
