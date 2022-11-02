16e3ebeae8d9f4fff2714dc5548a78eaf913c10f
Surbhi Gulati <53664026+surbhi-gulati@users.noreply.github.com>
2022-11-01 20:38:13 -0700

    Fix checkbox tests

--- components/questions/checkboxes-question/CheckboxesQuestion.test.tsx
+++ components/questions/checkboxes-question/CheckboxesQuestion.test.tsx
@@ -4,6 +4,20 @@ import CheckboxesQuestion from './CheckboxesQuestion';
 import { Checkboxes, QuestionResponse, QuestionType } from '../../../common/types';
 import { Form } from 'antd';
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 describe('CheckboxesQuestion component', () => {
   const n = (name: string) => ({ name });
   const exampleContent = 'Your top two most proficient languages. Select at least 1, but no more than 2.';
@@ -18,18 +32,20 @@ describe('CheckboxesQuestion component', () => {
     required: true,
   };
 
+  const CheckboxesQuestionWrapper = () => {
+    const form = Form.useForm<Record<string, QuestionResponse>>()[0];
+
+    return <CheckboxesQuestion question={exampleQuestion} form={form} disabled={false} />;
+  };
+
   it('renders', () => {
-    render(<CheckboxesQuestion 
-      question={exampleQuestion}
-      form={Form.useForm<Record<string, QuestionResponse>>()[0]}
-      disabled={false}
-    />);
+    render(<CheckboxesQuestionWrapper />);
     const confirmedDialogText = screen.getByTestId('checkboxes-question');
 
-    expect(confirmedDialogText).toContain(exampleContent);
-    expect(confirmedDialogText).toContain("English");
-    expect(confirmedDialogText).toContain("Java");
-    expect(confirmedDialogText).toContain("Typescript");
-    expect(confirmedDialogText).toContain("Japanese");
+    expect(confirmedDialogText.textContent).toContain(exampleContent);
+    expect(confirmedDialogText.textContent).toContain("English");
+    expect(confirmedDialogText.textContent).toContain("Java");
+    expect(confirmedDialogText.textContent).toContain("Typescript");
+    expect(confirmedDialogText.textContent).toContain("Japanese");
   });
 });
