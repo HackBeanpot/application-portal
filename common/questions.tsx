540f695b65f05398f3321d3a28bee147dc71e4b6
Dhivas Sugumar <dhivassugumar@gmail.com>
2023-11-13 19:33:44 -0500

    Fixed build errors

--- common/questions.tsx
+++ common/questions.tsx
@@ -430,7 +430,7 @@ export const Sections: Array<QuestionSection | QuestionDefinition> = [
     ],
     true,
   ),
-  makeDropdown(
+  makeRadioGroup(
     'ai',
     'AI/Machine Learning',
     [
@@ -440,9 +440,8 @@ export const Sections: Array<QuestionSection | QuestionDefinition> = [
       Familiarity.Expert,
     ],
     true,
-    'Familiarity'
   ),
-  makeDropdown(
+  makeRadioGroup(
     'productManagement',
     'Product Management',
     [
@@ -452,9 +451,8 @@ export const Sections: Array<QuestionSection | QuestionDefinition> = [
       Familiarity.Expert,
     ],
     true,
-    'Familiarity'
   ),
-  makeDropdown(
+  makeRadioGroup(
     'entrepreneurship',
     'Entrepreneurship',
     [
@@ -464,7 +462,6 @@ export const Sections: Array<QuestionSection | QuestionDefinition> = [
       Familiarity.Expert,
     ],
     true,
-    'Familiarity'
   ),
   makeSection(<></>, <i>Which CS disciplines are you interested in learning more about?</i>),
   makeRadioGroup(
@@ -508,23 +505,20 @@ export const Sections: Array<QuestionSection | QuestionDefinition> = [
     'Cybersecurity',
     Object.values(InterestLevel),
     true,
-    'Interest level'
   ),
-  makeDropdown(
+  makeRadioGroup(
     'aiInterestLevel',
     'Ai/Machine Learning',
     Object.values(InterestLevel),
     true,
-    'Interest level'
   ),
-  makeDropdown(
+  makeRadioGroup(
     'productManagementInterestLevel',
     'Product Management',
     Object.values(InterestLevel),
     true,
-    'Interest level'
   ),
-  makeDropdown(
+  makeRadioGroup(
     'entrepreneurshipInterestLevel',
     'Entrepreneurship',
     Object.values(InterestLevel),
