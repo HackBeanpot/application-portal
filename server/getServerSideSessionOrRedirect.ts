7a1e76891aef484fb606a2cf9c048ae73d8914c4
unknown <nelsondong158@gmail.com>
2024-11-11 16:10:33 -0500

    trest

--- server/getServerSideSessionOrRedirect.ts
+++ server/getServerSideSessionOrRedirect.ts
@@ -1,11 +1,14 @@
 import { getSession } from 'next-auth/react';
 import { GetServerSideProps } from 'next';
 import { SIGN_IN_PAGE } from '../pages/_app';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../pages/api/auth/[...nextauth]';
 
 export const getServerSideSessionOrRedirect: GetServerSideProps = async (
   ctx
 ) => {
-  const session = await getSession(ctx);
+  const {req, res} = ctx
+  const session = await getServerSession(req, res, authOptions)
   if (!session) {
     return {
       redirect: {
